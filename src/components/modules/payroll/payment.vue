d6c120ac22935411b05a96377a4e1e4229a760c1
unknown <piecesnbits@protonmail.com>
2020-07-06 14:21:47 +0200

    sync

--- src/components/modules/payroll/payment.vue
+++ src/components/modules/payroll/payment.vue
@@ -1,42 +1,79 @@
 <template>
-      <q-item v-if="payment" clickable>
-        <q-item-section avatar>
+
+
+
+      <q-expansion-item>
+        <template v-slot:header>
+          <q-item-section avatar>
             <profile-pic :size="42" icon="" iconColor="positive" :account="payment.receiver"  />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label class="text-capitalize">{{payment.receiver}}</q-item-label>
-          <q-item-label caption>ID {{payment.pay_id}}</q-item-label>
-        </q-item-section>
-        <q-item-section>
-          <q-item-label class="text-capitalize">Amount</q-item-label>
-          <q-item-label caption>{{payment.amount}}</q-item-label>
-        </q-item-section>
+          </q-item-section>
 
-        <q-item-section>
-          <q-item-label class="text-capitalize">Due date</q-item-label>
-          <q-item-label caption>{{payment.due_date}}</q-item-label>
-        </q-item-section>
-        <q-item-section>
-          <q-item-label class="text-capitalize">Repeat</q-item-label>
-          <q-item-label caption>
-            <span v-if="convert_recurrence_sec(payment.recurrence_sec)">{{convert_recurrence_sec(payment.recurrence_sec)}}</span>
-            <span>{{`${payment.repeated+1}/${payment.repeat}`}}</span>
-          </q-item-label>
-        </q-item-section>
+          <q-item-section>
+            <q-item-label class="text-capitalize">{{payment.receiver}}</q-item-label>
+            <q-item-label caption>ID {{payment.pay_id}}</q-item-label>
+          </q-item-section>
 
-        <q-item-section side>
-          <q-btn flat dense icon="delete" color="negative" size="sm">
-            <q-tooltip content-class="bg-secondary" :delay="500">
-              Propose removal from payroll
-            </q-tooltip>
-          </q-btn>
-        </q-item-section>
-      </q-item>
+          <q-item-section v-if="convert_recurrence_sec(payment.recurrence_sec)">
+            <span><q-badge >{{convert_recurrence_sec(payment.recurrence_sec)}}</q-badge></span>
+          </q-item-section>
+
+
+          <q-item-section side>
+            <div>
+              <span>{{payment.amount}}</span>
+              <span v-if="payment.repeat > 1" class="text-caption"> (x{{payment.repeat}})</span>
+            </div>
+            <!-- <div class="row items-center">
+              <q-icon name="star" color="red" size="24px" />
+              <q-icon name="star" color="red" size="24px" />
+              <q-icon name="star" color="red" size="24px" />
+            </div> -->
+          </q-item-section>
+        </template>
+
+        <q-card>
+          <q-card-section>
+            <!-- {{payment.due_date}} -->
+            <q-list class="full-width">
+              <q-item dense v-for="(p,i) in getClaimDates()" :key="p.claim_ms">
+                <q-item-section avatar>
+                  <q-badge><q-icon name="mdi-clock" size="16px" class="q-mr-xs" />{{`${i+1}/${payment.repeat}`}}</q-badge>
+                </q-item-section>
+                <q-item-section>
+                  {{p.claim_date}}
+                  
+                </q-item-section>
+                <q-item-section side>
+                  <div class="row items-center ">
+                    <q-icon v-if="p.claimable" name="mdi-check" color="positive" />
+                    <q-icon v-else name="mdi-close" color="negative" />
+                    <span>/</span>
+                    <q-icon v-if="p.is_claimed" name="mdi-check" color="positive" />
+                    <q-icon v-else name="mdi-close" color="negative" />
+                  </div>
+                </q-item-section>
+              </q-item>
+            </q-list>
+            <div class="text-right">
+              <q-btn label="delete" icon="delete" color="negative" >
+                <q-tooltip content-class="bg-secondary" :delay="500">
+                  Propose removal from payroll
+                </q-tooltip>
+              </q-btn>
+
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+
+
+
 </template>
 
 <script>
 import profilePic from "components/profile-pic";
 import {time_options} from "../../../imports/time_options";
+import { date } from 'quasar';
 export default {
   name: 'payment',
   props:{
@@ -51,6 +88,21 @@ export default {
   methods:{
     convert_recurrence_sec(sec){
       return time_options.get(sec);
+    },
+    getClaimDates(){
+      let res = [];
+      let now = Date.now();
+      let start = new Date(this.payment.due_date+".000+00:00").getTime();
+      for(let i = 0; i < this.payment.repeat; i++){
+        let cobj = {};
+        cobj.claim_ms = start + (i*this.payment.recurrence_sec*1000);
+        cobj.claim_date = date.formatDate(cobj.claim_ms, 'dddd Do MMMM YYYY HH:mm') //new Date(cobj.claim_ms);
+        cobj.claimable = cobj.claim_ms <= now;
+        cobj.ms_left = cobj.claim_ms - now;
+        cobj.is_claimed = this.payment.repeated > i;
+        res.push(cobj);
+      }
+      return res;
     }
     
   }
