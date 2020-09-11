d6b718d344eebedfa6b2cf56aa3bde029d8c67e0
unknown <piecesnbits@protonmail.com>
2020-09-11 10:52:11 +0200

    sync

--- src/imports/cosign/patches/anchor_patch.js
+++ src/imports/cosign/patches/anchor_patch.js
@@ -17,17 +17,51 @@ class anchor_patch{
     async patch(args){
       let trx = await this.getSerializedTrx(args[0]);
       //let trx = await this.userSign(args);
-    
-
       let cosignature = await this.getCoSignature(trx.serializedTransaction);
       trx.signatures = trx.signatures.concat(cosignature);
+
       let usersignature = await this.getUserSignature(trx.serializedTransaction);
       trx.signatures = trx.signatures.concat(usersignature);
+
       console.log("trx before push", trx)
 
       let completedTransaction =  await this.rpc.push_transaction(trx);
       console.log("completedTransaction",completedTransaction);
       return this.target.returnEosjsTransaction(true, completedTransaction)
+    }
+
+    async getSerializedTrx(transaction){
+      console.log("getSerializedTrx", "xxx")
+      let rpc = this.rpc;
+      const api = new Api({
+        rpc,
+        textDecoder: new TextDecoder(),
+        textEncoder: new TextEncoder()
+      });
+      console.log(api)
+      let res = await api.transact(
+        transaction, { broadcast : false, blocksBehind : 3, expireSeconds : 30, sign: false }
+      )
+      console.log("getSerializedTrx", res)
+      return res
+    }
+
+    async getCoSignature(serializedTransaction){
+
+      const signatureProvider = new JsSignatureProvider([this.options.priv_key]);
+      console.log("signatureProvider",signatureProvider)
+      const availablekeys = await signatureProvider.getAvailableKeys();
+      const signArgs = {
+        chainId: this.chainId,
+        requiredKeys: availablekeys,
+        serializedTransaction: serializedTransaction,
+        abis: [],
+      }
+      console.log(signArgs)
+      let signature =(await signatureProvider.sign(signArgs) ).signatures
+      console.log("cosignature", signature);
+      return signature;
+    
     }
 
     async getUserSignature(serializedTransaction){
@@ -52,38 +86,6 @@ class anchor_patch{
       )
       console.log("user signed", res)
       return res;
-    }
-
-    async getSerializedTrx(transaction){
-      let rpc = this.rpc;
-      const api = new Api({rpc});
-      console.log(api)
-      let res = await api.transact(
-        transaction, { broadcast : false, blocksBehind : 3, expireSeconds : 30, sign: false }
-      )
-      console.log("getSerializedTrx", res)
-      return res
-    }
-
-    async getCoSignature(serializedTransaction){
-
-
-      
-      const signatureProvider = new JsSignatureProvider([this.options.priv_key]);
-      console.log("signatureProvider",signatureProvider)
-      const availablekeys = await signatureProvider.getAvailableKeys();
-      const signArgs = {
-        chainId: this.chainId,
-        requiredKeys: availablekeys,
-        serializedTransaction: serializedTransaction,
-        abis: [],
-      }
-      console.log(signArgs)
-      let signature =(await signatureProvider.sign(signArgs) ).signatures
-      console.log("cosignature", signature);
-      return signature;
-    
-
     }
 }
 
