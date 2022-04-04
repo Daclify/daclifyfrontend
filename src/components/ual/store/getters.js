ad14965c36d924018ee03dc1e97d8092e36849e3
lukajs <lukasreichel73@gmail.com>
2022-04-04 17:46:10 +0900

    fix: available networks

--- src/components/ual/store/getters.js
+++ src/components/ual/store/getters.js
@@ -1,49 +1,49 @@
-export function getShouldRenderLoginModal (state) {
+export function getShouldRenderLoginModal(state) {
     return state.showLoginModal;
 }
 
-export function getSigningOverlay (state) {
+export function getSigningOverlay(state) {
     return state.signingOverlay;
 }
 
-export function getIsTransacting (state) {
+export function getIsTransacting(state) {
     return state.isTransacting;
 }
 
-export function getActiveAuthenticator (state) {
+export function getActiveAuthenticator(state) {
     return state.activeAuthenticator;
 }
 
-export function getAuthenticators (state) {
+export function getAuthenticators(state) {
     if (state.UAL) {
-        // return state.UAL.getAuthenticators().availableAuthenticators;
-        return state.UAL.authenticators;
+        return state.UAL.getAuthenticators().availableAuthenticators;
+        // return state.UAL.authenticators;
     }
     else {
         return [];
     }
 }
 
-export function getSESSION (state) {
+export function getSESSION(state) {
     return state.SESSION;
 }
 
-export function getUAL (state) {
+export function getUAL(state) {
     return state.UAL;
 }
 
-export function getAccountName (state) {
+export function getAccountName(state) {
     if (state.SESSION.accountName) {
         return state.SESSION.accountName;
     }
     return state.accountName;
 }
 
-export function getActiveNetwork (state) {
+export function getActiveNetwork(state) {
     return state.SESSION.network || state.activeNetwork;
 }
 
-export function getRpcEndpoints (state, getters) {
+export function getRpcEndpoints(state, getters) {
     let rpcobjs = state.networks[getters.getActiveNetwork].config.rpcEndpoints;
     return rpcobjs.map(rpcobj => {
         return rpcobj.protocol + "://" + rpcobj.host;
